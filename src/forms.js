589bb7b27822e11d40fcffd1d2e00860cd3321d7
Vincent <Vincentnguyen030@gmail.com>
2023-06-15 15:40:45 -0500

    Add 'add' and 'cancel' buttons on project creation

--- src/forms.js
+++ src/forms.js
@@ -1,11 +1,23 @@
 export function createProjectForm() {
     let form = document.createElement('div');
     form.classList.add('form');
+
     let name = document.createElement('input');
     name.type = 'text';
     name.classList.add('projectForm');
     name.placeholder = 'Enter project name';
-    form.append(name);
+
+    let buttons = document.createElement('div');
+    let add = document.createElement('button');
+    add.classList.add('add');
+    add.textContent = 'add';
+    let cancel = document.createElement('button');
+    cancel.classList.add('cancel');
+    cancel.textContent = 'cancel';
+    buttons.append(add,cancel);
+    buttons.style.cssText = 'display:grid;grid-template-columns:1fr 1fr;gap:1em';
+
+    form.append(name,buttons);
     
     return form;
 }
